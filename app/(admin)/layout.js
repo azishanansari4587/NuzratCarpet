c35d5f6b2bf74b9a6fc5c4c23873bf2619ea7754
Zishan Ansari <azishanansari4587@gmail.com>
2024-12-07 19:03:12 +0530

    fix

--- app/(admin)/layout.js
+++ app/(admin)/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
-import SideMenu from "@/components/SideMenu";
+import SideMenu from "@/components/Sidemenu";
 import NavbarDashboard from "@/components/NavbarDashboard";
 
 const inter = Inter({ subsets: ["latin"] });
